da6bbbd455f094100fa920345c477fd070f3cd7b
Olatbossun <boyxdik@gmail.com>
2023-08-05 23:05:04 +0100

    initial commit

---
+++ React-Template/src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App () {
+  return (
+    <>
+        <h1>hello world </h1>
+        <h3>This is a basic React Template application starting point for builbing react </h3>
+    </>
+  )
+}
+
+export default App
